916b38e99608317ae66d2fdcbaa59acc67d028cf
Nicolas <109356478+Neocraft1293@users.noreply.github.com>
2023-12-14 21:49:40 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,20 +1,5 @@
-local function print_to_everything(msg)
-    msg = msg or "nil message"
-    print("Debug - Message: " .. msg)  -- Ajoutez cette ligne pour le débogage
-    minetest.log("action", "[chest_recovery] " .. msg)
-    minetest.chat_send_all(msg)
-end
-
-local function print_to_everything(msg)
-    msg = msg or "nil message"
-    print("Debug - Message: " .. msg)  -- Ajoutez cette ligne pour le débogage
-    minetest.log("action", "[chest_recovery] " .. msg)
-    minetest.chat_send_all(msg)
-end
-
 minetest.register_node("chest_recovery:chest", {
-    description = "cofre recovery" .. "\n" ..
-        "32 inventory slots",
+    description = "Coffre de récupération" .. "\n" .. "32 emplacements",
     tiles = {"chest_chest.png^[sheet:2x2:0,0", "chest_chest.png^[sheet:2x2:0,0",
         "chest_chest.png^[sheet:2x2:1,0", "chest_chest.png^[sheet:2x2:1,0",
         "chest_chest.png^[sheet:2x2:1,0", "chest_chest.png^[sheet:2x2:0,1"},
@@ -24,55 +9,45 @@ minetest.register_node("chest_recovery:chest", {
     on_construct = function(pos)
         local meta = minetest.get_meta(pos)
         meta:set_string("formspec",
-            "size[8,9]"..
-            "list[current_name;main;0,0;8,4;]"..
-            "list[current_player;main;0,5;8,4;]" ..
+            "size[9,10]"..
+            "list[current_name;main;0,0;9,4;]"..
+            "list[current_name;armor;0,4;9,1;]"..
+            "list[current_player;armor;0,5;9,1;]"..
+            "list[current_player;main;0,6;9,4;]"..
             "listring[]")
-        meta:set_string("infotext", "Chest")
+        meta:set_string("infotext", "Coffre")
         local inv = meta:get_inventory()
-        inv:set_size("main", 8 * 4)
+        inv:set_size("main", 9 * 4)
+        inv:set_size("armor", 9)
     end,
-    can_dig = function(pos, player)
-        local meta = minetest.get_meta(pos);
-        local inv = meta:get_inventory()
-        return inv:is_empty("main")
-    end,
-    allow_metadata_inventory_put = function(pos, listname, index, stack, player)
-        print_to_everything("Chest: " .. player:get_player_name() .. " triggered 'allow put' event for " .. stack:to_string())
-        return stack:get_count()
-    end,
-    allow_metadata_inventory_take = function(pos, listname, index, stack, player)
+    on_destruct = function(pos)
         local meta = minetest.get_meta(pos)
         local inv = meta:get_inventory()
-        if inv:is_empty("main") then
-            -- Le coffre est maintenant vide, supprimez-le
-            minetest.remove_node(pos)
+        local drop_items = {}
+
+        for _, listname in ipairs({"main", "armor", "craft"}) do
+            for i = 1, inv:get_size(listname) do
+                local stack = inv:get_stack(listname, i)
+                if not stack:is_empty() then
+                    table.insert(drop_items, stack:to_table())
+                end
+            end
         end
-        print_to_everything("Chest: " .. player:get_player_name() .. " triggered 'allow take' event for " .. stack:to_string())
-        return stack:get_count()
-    end,
-    on_metadata_inventory_put = function(pos, listname, index, stack, player)
-        print_to_everything("Chest: " .. player:get_player_name() .. " put " .. stack:to_string())
-    end,
-    on_metadata_inventory_take = function(pos, listname, index, stack, player)
-        local meta = minetest.get_meta(pos)
-        local inv = meta:get_inventory()
-        if inv:is_empty("main") then
-            -- Le coffre est maintenant vide, supprimez-le
-            minetest.remove_node(pos)
+
+        -- Lâcher tous les objets au-dessus du nœud du coffre
+        for _, item in ipairs(drop_items) do
+            minetest.add_item(pos, ItemStack(item))
         end
-        print_to_everything("Chest: " .. player:get_player_name() .. " took " .. stack:to_string())
     end,
+    -- (le reste du code reste inchangé)
 })
 
+-- Reste du code...
 
-
-
--- Ajout de la gestion de la suppression si le coffre est vide lors de la mort du joueur
 minetest.register_on_dieplayer(function(player)
     local player_inv = player:get_inventory()
     local pos = player:get_pos()
-    pos.y = pos.y -- 0.5
+    pos.y = pos.y
 
     minetest.set_node(pos, {name = "chest_recovery:chest"})
 
@@ -81,30 +56,25 @@ minetest.register_on_dieplayer(function(player)
 
     local is_empty = true
 
-    for i = 1, player_inv:get_size("main") do
-        local stack = player_inv:get_stack("main", i)
-        chest_inv:set_stack("main", i, stack)
-        player_inv:set_stack("main", i, ItemStack(nil))
-        if not stack:is_empty() then
-            is_empty = false
-        end
-    end
-
-    for i = 1, player_inv:get_size("craft") do
-        local stack = player_inv:get_stack("craft", i)
-        chest_inv:set_stack("craft", i, stack)
-        player_inv:set_stack("craft", i, ItemStack(nil))
-        if not stack:is_empty() then
-            is_empty = false
+    for _, listname in ipairs({"main", "armor", "craft"}) do
+        for i = 1, player_inv:get_size(listname) do
+            local stack = player_inv:get_stack(listname, i)
+            chest_inv:set_stack(listname, i, stack)
+            player_inv:set_stack(listname, i, ItemStack(nil))
+            if not stack:is_empty() then
+                is_empty = false
+            end
         end
     end
 
     if is_empty then
         minetest.remove_node(pos)
     else
-        local chest_formspec = "size[8,9]" ..
-                               "list[current_name;main;0,0;8,4;]" ..
-                               "list[current_player;main;0,5;8,4;]" ..
+        local chest_formspec = "size[9,10]" ..
+                               "list[current_name;main;0,0;9,4;]" ..
+                               "list[current_name;armor;0,4;9,1;]" ..
+                               "list[current_player;armor;0,5;9,1;]" ..
+                               "list[current_player;main;0,6;9,4;]" ..
                                "listring[]"
         chest_meta:set_string("formspec", chest_formspec)
     end
